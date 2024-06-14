74b1cfd3c68e15214d29f7b5429dee5da37a01c3
Yufan Zhang <YZhang@rpmglobal.com>
2024-06-14 23:49:18 +1000

    Add clear functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
-console.log(1);
+let buttons = document.querySelector('.buttons');
+let display = document.querySelector('.display');
+let clear = document.querySelector('.clear');
+
+buttons.addEventListener('click', (e) => {
+    let text = document.createTextNode(e.target.textContent);
+    if (e.target.textContent === 'AC') {
+        return;
+    }
+
+    if (display.childNodes.length > 11) {
+        return;
+    }
+
+    display.appendChild(text);
+});
+
+clear.addEventListener('click', () => {
+    while (display.firstChild) {
+        display.removeChild(display.firstChild);
+    }
+});
